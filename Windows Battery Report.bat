6f94ba952d5a3e7b472a97e88206eea9d43c11fc
Yonatan Israeli <yonirisraeli@gmail.com>
2024-03-11 20:28:27 -0400

    Update Windows Battery Report.bat

--- Windows Battery Report.bat
+++ Windows Battery Report.bat
@@ -1,7 +1,8 @@
 @echo off
 setlocal
 title CMD Windows Battery Report
-echo Please run this batch file as an administrator.
+echo Please run this batch file as an administrator. Press any key to start.
+pause >nul
 goto :Start
 
 :Start
