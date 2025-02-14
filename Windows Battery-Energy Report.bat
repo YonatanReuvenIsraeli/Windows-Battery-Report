41a74b01c9a81fa1cf1762eaed10144a9f1c7ba1
Yonatan Israeli <yonirisraeli@gmail.com>
2025-02-14 01:05:14 -0500

    Update Windows Battery-Energy Report.bat

--- Windows Battery-Energy Report.bat
+++ Windows Battery-Energy Report.bat
@@ -2,7 +2,7 @@
 title Windows Battery/Energy Report
 setlocal
 echo Program Name: Windows Battery/Energy Report
-echo Version: 2.0.4
+echo Version: 2.0.5
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
@@ -96,10 +96,10 @@ goto "EnergyReport"
 :"EnergyReport"
 if exist "energy-report.html" goto "EnergyReportExist"
 echo.
-"%windir%\System32\powercfg.exe" /energy /duration %Duration%
+echo Starting energy test at %Date% %Time%. You will have to wait %Duration% second(s) for the test to be complete. Do not close this batch file.
+"%windir%\System32\powercfg.exe" /energy /duration %Duration% > nul 2>&1
 if not "%errorlevel%"=="0" goto "Error"
 "energy-report.html"
-echo.
 echo Press any key to delete your energy report and close this batch file.
 pause > nul 2>&1
 del "energy-report.html" /f /q > nul 2>&1
